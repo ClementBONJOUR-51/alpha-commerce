79732c644a72f3ea9e8445be8f7e2cb5bf730494
Jason Noel <jason.noel.imp@gmail.com>
2019-07-17 15:49:50 +0200

    Car validates in model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
     has_one_attached :main_picture
+    #Active storage has no attached validator for now so this is home made
+    validates :main_picture, attached: true
+
+    validates :horsepower, :energy, :gearbox, :mileage, :price, :description, :model, presence: true
+    validates :model, length: {minimum: 3, maximum: 70}
+    validates :description, length: {minimum: 10}
 end
